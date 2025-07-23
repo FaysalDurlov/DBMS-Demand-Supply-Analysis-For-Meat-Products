7a93c54840aebdd1dd8f0721e17fe04f39306275
shihab-sani <mdsani633@gmail.com>
2025-07-24 03:21:44 +0600

    added animal, sales, puchase dummy data (edit: none)(dummy data Folder)

---
+++ Meatrix/dammy data/salesData.js
@@ -0,0 +1,42 @@
+const salesData = [
+  {
+    saleAnimalId: "A1001", 
+    saleWeight: 480, 
+    salePrice: 1250,
+    buyerName: "Green Butchery", 
+    buyerContact: "01711111111", 
+    saleDate: "2025-07-20"
+  },
+  {
+    saleAnimalId: "A1002", 
+    saleWeight: 65, 
+    salePrice: 280,
+    buyerName: "Local Market", 
+    buyerContact: "01722222222", 
+    saleDate: "2025-07-18"
+  },
+  {
+    saleAnimalId: "A1003", 
+    saleWeight: 55, 
+    salePrice: 240,
+    buyerName: "Sheep Farmers Co-op", 
+    buyerContact: "01733333333", 
+    saleDate: "2025-07-16"
+  },
+  {
+    saleAnimalId: "A1004", 
+    saleWeight: 530, 
+    salePrice: 1350,
+    buyerName: "Beef Palace", 
+    buyerContact: "01744444444", 
+    saleDate: "2025-07-14"
+  },
+  {
+    saleAnimalId: "A1005", 
+    saleWeight: 60, 
+    salePrice: 260,
+    buyerName: "Goat Meat Ltd.", 
+    buyerContact: "01755555555", 
+    saleDate: "2025-07-10"
+  }
+];

ea3a36a8cc59f1e2c36cbaa1540338336bfa8087
FaysalDurlov <foysaldurlov@gmail.com>
2025-07-25 17:22:53 +0600

    UserInfo_usertype_fileStructured

---
+++ Meatrix/dammy data/UserType.js
@@ -0,0 +1,7 @@
+export const USER_TYPES = [
+    { value: "farmer", label: "Farmer" },
+    { value: "veterinarian", label: "Veterinarian" },
+    { value: "supplier", label: "Supplier" },
+    { value: "manager", label: "Farm Manager" },
+    { value: "inspector", label: "Inspector" },
+]

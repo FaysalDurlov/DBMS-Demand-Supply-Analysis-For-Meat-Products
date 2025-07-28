cc09732aada480a648b11590f4f719e6326e9e41
FaysalDurlov <foysaldurlov@gmail.com>
2025-07-28 18:11:06 +0600

    all_button_Added_eventListener_&_Added_aCommon_feature_file_for_all_users

---
+++ Meatrix/Scripts/CommonFeatureScript.js
@@ -0,0 +1,4 @@
+export function LogOut(){
+    // window.location.href = "../LogIn/LoginPage.html";   | problem is it can still use back button on browser
+    window.location.replace("../LogIn/LoginPage.html");
+}

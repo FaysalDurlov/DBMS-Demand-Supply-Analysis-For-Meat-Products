63c25c665dd5b5f829dad339e4aab15031d83afc
FaysalDurlov <foysaldurlov@gmail.com>
2025-08-29 18:19:43 +0600

    stable connection with database_password_updated

--- Meatrix/TESTING/app.js
+++ Meatrix/TESTING/app.js
@@ -3,6 +3,9 @@ import mysql from "mysql2/promise";
 const db = await mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Shihab14032001",
+  password: "Bolbona15203118",
+  // Bolbona15203118
+  // Shihab14032001
   database: "livestockdb",
 });
+console.log("Connected to the database.");
